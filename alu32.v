3d35c4dff2adf411ab3761c73503ceb98ba03c3e
caglasen <62358236+caglasen@users.noreply.github.com>
2020-06-13 08:54:57 +0300

    SLL eklendi, nout eklendi

--- alu32.v
+++ alu32.v
@@ -1,10 +1,11 @@
-module alu32(sum,a,b,zout,gin);//ALU operation according to the ALU control line values
+module alu32(sum,a,b,shamt,zout,nout,gin);//ALU operation according to the ALU control line values. Negative(nout) outputu eklendi*******************
 output [31:0] sum;
 input [31:0] a,b; 
+input [4:0] shamt;
 input [2:0] gin;//ALU control line
 reg [31:0] sum;
 reg [31:0] less;
-output zout;
+output zout,nout; //nout eklendi************
 reg zout;
 always @(a or b or gin)
 begin
@@ -17,8 +18,10 @@ begin
 		  end
 	3'b000: sum=a & b;	//ALU control line=000, AND
 	3'b001: sum=a|b;		//ALU control line=001, OR
+	3'b011: sum=b<<shamt;	//ALU control line=011, SLL eklendi*******************
 	default: sum=31'bx;	
-	endcase
+endcase
 zout=~(|sum);
+nout==sum[31]; //if the first bit is 1, then this means result is negative**********************
 end
 endmodule
