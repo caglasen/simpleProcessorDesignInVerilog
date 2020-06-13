7b384476af49395cd78ffb55442151cf8a8ac771
caglasen <62358236+caglasen@users.noreply.github.com>
2020-06-13 09:48:16 +0300

    baya bi ekleme yapıldı.
    instruction sayısı çoğaltıldı mem[0:511].
    Çıkan andi sinyaline göre zero extend mi yoksa sign extend mi yapacağımıza karar verdiğimiz bir mux ekledik(mult5). Bu muxun sonucu da signexteddORzeroextended.
    aluya jmorsigg ekledik.
    shamtt shift amount ekledik, bunu aluya gönderdik.
    jal instructionunda kullanacağımız targett'ı ekledik. Bu da instruc[25:0]a eşit.
    
    mult2 muxu signexteddORzeroextended ve datab'yi alıyor ve alusrc'nin 1 olup olmamasına bağlı olarak out2 outputunu veriyor.
    
    yeni bir adder eklendi(add3). pc+4 ile targett' i topluyor.
    
    jumpbrcontrol uniti eklendi.
    
    4x1 mux eklendi. Parametreleri DÜZENLENECEK!!!!

--- processor.v
+++ processor.v
@@ -2,7 +2,7 @@ module processor;
 reg [31:0] pc; //32-bit prograom counter
 reg clk; //clock
 reg [7:0] datmem[0:31];
-reg [7:0] mem[0:31]; //32-size data and instruction memory (8 bit(1 byte) for each location)
+reg [7:0] mem[0:511]; //32-size data and instruction memory (8 bit(1 byte) for each location)
 wire [31:0] 
 dataa,	//Read data 1 output of Register File
 datab,	//Read data 2 output of Register File
@@ -12,8 +12,11 @@ out4,		//Output of mux with (Branch&ALUZero) control-mult4
 sum,		//ALU result
 extad,	//Output of sign-extend unit
 adder1out,	//Output of adder which adds PC and 4-add1
-adder2out,	//Output of adder which adds PC+4 and 2 shifted sign-extend result-add2
-sextad;	//Output of shift left 2 unit
+adder2out,
+adder3out,	//Output of adder which adds PC+4 and 2 shifted sign-extend result-add2
+sextad, //Output of shift left 2 unit
+zextad,//Output of zero extend unit
+signexteddORzeroextended;	
 
 wire [5:0] inst31_26;	//31-26 bits of instruction
 wire [4:0] 
@@ -29,10 +32,14 @@ dpack;	//Read data output of memory (data read from memory)
 
 wire [2:0] gout;	//Output of ALU control unit
 
+
+
 wire zout,	//Zero output of ALU
+nout,
+jmorsigg, //negative output of ALU******************************
 pcsrc,	//Output of AND gate with Branch and ZeroOut inputs
 //Control signals
-regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,aluop2,aluop1,aluop0,andisignal;
+regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,aluop2,aluop1,aluop0,andisign, blezsign, jalsing, jmorsign, balrnsign, sllsign;
 
 //32-size register file (32 bit(1 word) for each register)
 reg [31:0] registerfile[0:31];
@@ -60,7 +67,12 @@ end
  assign inst20_16=instruc[20:16];
  assign inst15_11=instruc[15:11];
  assign inst15_0=instruc[15:0];
+ 
+wire[4:0] shamtt;//***************************************************SHIFT AMOUNT FOR ALU
+assign shamtt=instruc[10:6];//****************************************SHIFT AMOUNT FOR ALU
 
+wire [25:0] targett;//*****************************************  JAL INST.da KULLANILACAK OLAN TARGET
+assign targett=instruc[25:0];//********************************
 
 // registers
 
@@ -76,8 +88,11 @@ assign dpack={datmem[sum[5:0]],datmem[sum[5:0]+1],datmem[sum[5:0]+2],datmem[sum[
 //mux with RegDst control
 mult2_to_1_5  mult1(out1, instruc[20:16],instruc[15:11],regdest);
 
+// input olarak sign extended imm. ve zero extended imm., select bit olarak andisignal alan bir mux eklendi********************************
+mult2_to_1_32 mult5(signexteddORzeroextended, extad, zextad, andisign);
+
 //mux with ALUSrc control
-mult2_to_1_32 mult2(out2, datab,extad,alusrc);
+mult2_to_1_32 mult2(out2, datab,signexteddORzeroextended,alusrc);
 
 //mux with MemToReg control
 mult2_to_1_32 mult3(out3, sum,dpack,memtoreg);
@@ -85,30 +100,45 @@ mult2_to_1_32 mult3(out3, sum,dpack,memtoreg);
 //mux with (Branch&ALUZero) control
 mult2_to_1_32 mult4(out4, adder1out,adder2out,pcsrc);
 
+
+
 // load pc
 always @(negedge clk)
 pc=out4;
 
 // alu, adder and control logic connections
 
-//ALU unit
-alu32 alu1(sum,dataa,out2,zout,gout);
+//ALU unit operate on dataa and out2
+alu32 alu1(sum,dataa,out2,shamtt,zout,nout, jmorsigg, gout); //nout eklendi****************
 
 //adder which adds PC and 4
-adder add1(pc,32'h4,adder1out);
+adder add1(pc,32'h4,adder1out); //pc+4 , MESELA JMOR PC+4e GİDECEK
 
-//adder which adds PC+4 and 2 shifted sign-extend result
-adder add2(adder1out,sextad,adder2out);
+//adder with result PC + 4 + LABEL ***************************************     ????????????????????????????????????????????????????      
+adder add3(adder1out,targett,adder3out);
+
+//adder which adds PC+4 and 2 shifted sign-extend result 			BU BLEZ İÇİN KULLANILACAK
+adder add2(adder1out,sextad,adder2out); // adder2out= PC + 4 + 4*LABEL
+
+wire [1:0] cont;
+
+jumpbrcontrol jumpandbranchcontrolUnit(beq,balrn,blez,jal,jmor,nout,zout,cont); // BURASI DEĞİŞECEK!!!
+
+//4x1 mux for choosing the jump destinastion
+mult4_to_1_32 mult7(outDestination,????,adder2out,adder1out,sextad,cont);
 
 //Control unit
-control cont(instruc[3:0],instruc[31:26], regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,aluop2,
-aluop1,aluop0,andisignal);
+control cont(instruc[5:0],instruc[31:26], regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,andisign, blezsign, jalsing, sllsign, jmorsign, balrnsign, aluop2, //fcode 6 bite çıktı(ilk arguman)
+aluop1,aluop0);//buraya andisignal eklendi
 
 //Sign extend unit
 signext sext(instruc[15:0],extad);
 
+//andi için zero extend unit eklendi
+zeroextend zext(instruc[15:0],zextad);
+
 //ALU control unit
-alucont acont(aluop2, aluop1,aluop0,instruc[3],instruc[2], instruc[1], instruc[0] ,andisignal,gout);
+alucont acont(aluop2, aluop1,aluop0,instruc[5],instruc[4],instruc[3],instruc[2], instruc[1], instruc[0] ,gout);
 
 //Shift-left 2 unit
 shift shift2(sextad,extad);
@@ -133,11 +163,13 @@ initial
 begin
 pc=0;
 //#400 $finish;
+#2000 $finish;
+
 	
 end
 initial
 begin
-clk=0;
+clk=1;
 //40 time unit for each cycle
 forever #20  clk=~clk;
 end
