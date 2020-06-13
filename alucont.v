93671dca3cfe1ef65be1a492f0156ce1c199d7f2
caglasen <62358236+caglasen@users.noreply.github.com>
2020-06-13 08:58:01 +0300

    jmor, +2 function code biti,
    jmor output sinyali eklendi.
    jmorun function code'undan or sinyali çıkardık.
    function code'a 2 bit eklendi.

--- alucont.v
+++ alucont.v
@@ -1,12 +1,19 @@
-module alucont(aluop1,aluop0,f3,f2,f1,f0,andisignal,gout);//Figure 4.12 
-input aluop1,aluop0,f3,f2,f1,f0,andisignal;
+module alucont(aluop2,aluop1,aluop0,f5,f4,f3,f2,f1,f0,jmorsig,gout);//Figure 4.12 //2 bit function code eklendi, jmor sinyali eklendi
+input aluop2,aluop1,aluop0,f5,f4,f3,f2,f1,f0; //2 bit function code eklendi
 output [2:0] gout;
+output jmorsig; //ALUCONTROLDEN JMOR SİNYALİ ÇIKARIYORUZ
 reg [2:0] gout;
-always @(aluop1 or aluop0 or f3 or f2 or f1 or f0)
+always @(aluop2,aluop1 or aluop0 or f5 or f4 or f3 or f2 or f1 or f0) //2 bit function code eklendi
 begin
 if(~(aluop1|aluop0))  gout=3'b010;
 if(aluop0)gout=3'b110;
-if(andisignal) gout=3'b000; //andisignal
+if(aluop2) gout=3'b000; // aluop2 =1 ise andi inst. demektir.
+if(aluop1 & ~aluop0) gout=3'b011; // aluop of sll is x10
+if(f5 & ~f4 & ~f3 & f2 & ~f1 & f0) 
+	begin
+		gout=3'b001 
+		jmorsig=1'b1; // fcode of jmor is 100101
+	end
 if(aluop1)//R-type
 begin
 	if (~(f3|f2|f1|f0))gout=3'b010; 	//function code=0000,ALU control=010 (add)
@@ -14,6 +21,7 @@ begin
 	if (f1&~(f3))gout=3'b110;		//function code=0x10,ALU control=110 (sub)
 	if (f2&f0)gout=3'b001;			//function code=x1x1,ALU control=001 (or)
 	if (f2&~(f0))gout=3'b000;		//function code=x1x0,ALU control=000 (and)
+	if (~(f5)&~(f4)&~(f3)&~(f2)&~(f1)&~(f0)) gout=3'b011; //function code=000000, ALU control=011 (sll)*******************????????????????***
 end
 end
 endmodule
